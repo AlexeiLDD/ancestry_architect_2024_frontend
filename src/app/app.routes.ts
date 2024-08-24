060ca25630c1eff5aa6e3f9959580e327a523b0e
AlexeiLDD <doroga218@gmail.com>
2024-08-25 01:29:38 +0300

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { SignupComponent } from './auth/pages/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth/login',
+    title: 'Вход',
+    component: LoginComponent,
+  },
+  {
+    path: 'auth/signup',
+    title: 'Регистрация',
+    component: SignupComponent,
+  },
+];
