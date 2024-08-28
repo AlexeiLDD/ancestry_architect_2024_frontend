5db2de9cd5855ccf84b3f1d6695c44bb3915176d
AlexeiLDD <doroga218@gmail.com>
2024-08-28 22:52:15 +0300

    add cors proxy config

--- src/app/core/models/http-base.ts
+++ src/app/core/models/http-base.ts
@@ -1,10 +1,10 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export class HttpBase {
-  public baseURL: string = 'http://localhost:8080/api/';
+  public baseURL: string = '/api/';
   public baseHeaders: HttpHeaders = new HttpHeaders({ 
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+    // 'Access-Control-Allow-Origin': '*',
+    // 'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT, HEAD, OPTIONS',
+    // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
   });
 }
