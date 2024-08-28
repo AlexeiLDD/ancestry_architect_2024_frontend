25d2c34a27022d3ef29180e784c499faa7ff0a08
AlexeiLDD <doroga218@gmail.com>
2024-08-28 22:50:41 +0300

    added request and response structures

--- src/app/core/models/request.ts
+++ src/app/core/models/request.ts
@@ -1,2 +1,13 @@
 
+export namespace Request {
+    export namespace Auth {
+        export interface Login {
+            email: string;
+            password: string;
+        }
 
+        export interface Signup extends Login {
+            passwordRepeat: string;
+        }
+    }
+}
--- src/app/core/models/response.ts
+++ src/app/core/models/response.ts
@@ -1,6 +1,14 @@
 import { HttpStatusCode } from "@angular/common/http"
+import { UserInterace } from "./auth"
 
-export interface Response<T>{
+export interface Response<T> {
     code: HttpStatusCode,
     payload: T
+}
+
+export interface UserResponse {
+    isAuth: boolean,
+    user: UserInterace,
+    name: string,
+    surname: string,
 }
