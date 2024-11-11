8f3450c160dfcb73a480215fba02696a1c676655
AlexeiLDD <doroga218@gmail.com>
2024-11-11 20:45:25 +0300

    added tree-service get and create methods

--- src/app/tree/services/tree/tree.service.ts
+++ src/app/tree/services/tree/tree.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpBase } from '../../../core/models/http-base';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Response, SuccessResponse, TreeListResponse } from '../../../core/models/response';
+import { CreateTreeResponse, GetTreeResponse, Response, SuccessResponse, TreeListResponse } from '../../../core/models/response';
 import { Request } from '../../../core/models/request';
 
 @Injectable({
@@ -10,6 +10,13 @@ import { Request } from '../../../core/models/request';
 })
 export class TreeService extends HttpBase{
   constructor(private http: HttpClient) { super(); }
+
+  public get(id: number): Observable<Response<GetTreeResponse>> {
+    return this.http.get<Response<GetTreeResponse>>(
+      `${this.baseURL}tree/${id}`,
+      { headers: this.baseHeaders },
+    );
+  }
 
   public getCreatedList(): Observable<Response<TreeListResponse>> {
     return this.http.get<Response<TreeListResponse>>(
@@ -28,6 +35,14 @@ export class TreeService extends HttpBase{
   public grantPermission(request: Request.Tree.GrantPermission): Observable<Response<SuccessResponse>> {
     return this.http.post<Response<SuccessResponse>>(
       `${this.baseURL}tree/permission`,
+      request,
+      { headers: this.baseHeaders },
+    );
+  }
+  
+  public create(request: Request.Tree.CreateTree): Observable<Response<CreateTreeResponse>> {
+    return this.http.post<Response<CreateTreeResponse>>(
+      `${this.baseURL}tree`,
       request,
       { headers: this.baseHeaders },
     );
