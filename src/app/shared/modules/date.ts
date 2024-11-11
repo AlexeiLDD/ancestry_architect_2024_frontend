ee16a64f6f8ffc2a2018f3ac944597dbb51379a5
AlexeiLDD <doroga218@gmail.com>
2024-11-11 20:47:37 +0300

    added formatDateToPreview funk

--- src/app/shared/modules/date.ts
+++ src/app/shared/modules/date.ts
@@ -3,4 +3,8 @@ export namespace DateModule{
     export function formatDate(date: string): string {
         return new Date(date).toLocaleDateString("en-GB");
     }
+
+    export function formatDateToPreview(date: string): string {
+        return new Date(date).toLocaleDateString("ru-RU");
+    }
 }
