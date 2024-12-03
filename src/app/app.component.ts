7938d6f68c573879b32eb8eadb1d2eb11e171e7d
AlexeiLDD <doroga218@gmail.com>
2024-12-03 12:48:50 +0300

    added main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,30 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterOutlet, UrlSegment } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { UserService } from './core/services/user/user.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Ancestry Architect';
+  isMain = true;
 
   @ViewChild('elem') elem?: ElementRef;
 
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService, 
+    private route: ActivatedRoute,
+  ) {}
 
   isGotUser(): boolean {
     return this.usersService.userIsFetched;
   }
+
+  activateRouter() { this.isMain = false; }
+  deactivateRouter() { this.isMain = true; }
 }
